8bc3136ffc63d46e903e5cb20ba1aea2b483b3a0
JacbGit <127043001+JacbGit@users.noreply.github.com>
2025-10-14 07:58:54 -0600

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'nestuser',
+      password: 'admin',
+      database: 'DEV_CR',
+      entities: [__dirname+'/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      options:{
+        encrypt: false,
+        enableArithAbort: true,
+      }
+    }),
+    UsersModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ backend/src/users/user.entity.ts
@@ -0,0 +1,31 @@
+import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn } from "typeorm";
+
+@Entity('Users')
+export class User {
+    @PrimaryGeneratedColumn()
+    UserID: number;
+
+    @Column({ length: 63 })
+    Name: string;
+
+    @Column({length: 63})
+    Surname: string;
+
+    @Column({length: 63})
+    Username: string;
+
+    @Column({length: 63})
+    Password: string;
+
+    @Column()
+    Status: number;
+
+    @Column({length: 255, nullable:true})
+    Photo: string;
+
+    @Column()
+    UserType: number;
+
+    @CreateDateColumn({type: 'datetime'})
+    CreatedAt: Date;
+}
